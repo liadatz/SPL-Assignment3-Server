94e3e09642edf41d6c539c02c560f32227569d99
Liad Atsmon <liad.atsmon@gmail.com>
2020-12-20 20:57:40 +0200

    added course

--- src/main/java/bgu/spl/net/impl/passiveObjects/Course.java
+++ src/main/java/bgu/spl/net/impl/passiveObjects/Course.java
@@ -1,4 +1,47 @@
 package bgu.spl.net.impl.passiveObjects;
 
+import java.util.List;
+
 public class Course {
+    int courseNum;
+    String courseName;
+    List<Course> KdamCoursesList;
+    int numOfMaxStudents;
+    int numOfRegistered;
+
+    public Course(int courseNum, String courseName, List<Course> kdamCoursesList, int numOfMaxStudents, int numOfRegistered) {
+        this.courseNum = courseNum;
+        this.courseName = courseName;
+        KdamCoursesList = kdamCoursesList;
+        this.numOfMaxStudents = numOfMaxStudents;
+        this.numOfRegistered = numOfRegistered;
+    }
+
+    public int getCourseNum() {
+        return courseNum;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public List<Course> getKdamCoursesList() {
+        return KdamCoursesList;
+    }
+
+    public int getNumOfMaxStudents() {
+        return numOfMaxStudents;
+    }
+
+    public int getNumOfRegistered() {
+        return numOfRegistered;
+    }
+
+    public void increaseNumOfRegistered(){
+        if (numOfRegistered < numOfMaxStudents) numOfRegistered++;
+    }
+
+
 }
+
+
