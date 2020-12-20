dbb6e8ec183a166f770f1df3926859a834e2cb73
omerbinyamin4 <63786735+omerbinyamin4@users.noreply.github.com>
2020-12-20 20:53:03 +0200

    Studens class

--- src/main/java/bgu/spl/net/impl/passiveObjects/Student.java
+++ src/main/java/bgu/spl/net/impl/passiveObjects/Student.java
@@ -1,4 +1,38 @@
 package bgu.spl.net.impl.passiveObjects;
 
+import java.util.List;
+
 public class Student {
+/*---------------------------------fields---------------------------------*/
+    private String username;
+    private String password;
+    private List<Course>  courses;
+/*-------------------------------constructors------------------------------*/
+    public Student(String username, String password, List<Course> courses){
+        this.username = username;
+        this.password = password;
+        this.courses = courses;
+    }
+/*---------------------------------getters---------------------------------*/
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public List<Course> getCourses() {
+        return courses;
+    }
+/*---------------------------------methods---------------------------------*/
+    public void addCourse(Course toAdd){
+        if (toAdd != null)
+            courses.add(toAdd);
+    }
+
+    public boolean checkCourse(Course toCheck){
+        if (toCheck != null)
+            return (courses.contains(toCheck));
+        return false;
+    }
+
 }
